9400b0b2893b9db1aec32ea215df7b1d860ed0eb
Milad Nekofar <milad@nekofar.com>
2023-03-30 10:05:56 +0330

    feat: implement directory project generator

---
+++ src/main/kotlin/com/nekofar/milad/intellij/blitz/BlitzBundle.kt
@@ -0,0 +1,13 @@
+package com.nekofar.milad.intellij.blitz
+
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+
+@NonNls
+private const val BUNDLE = "messages.BlitzBundle"
+
+object BlitzBundle : DynamicBundle(BUNDLE) {
+    @JvmStatic
+    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) = getMessage(key, *params)
+}
---
+++ src/main/kotlin/com/nekofar/milad/intellij/blitz/cli/BlitzCliProjectGenerator.kt
@@ -0,0 +1,55 @@
+package com.nekofar.milad.intellij.blitz.cli
+
+import com.intellij.execution.filters.Filter
+import com.intellij.lang.javascript.boilerplate.NpmPackageProjectGenerator
+import com.intellij.lang.javascript.boilerplate.NpxPackageDescriptor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ContentEntry
+import com.intellij.openapi.vfs.VirtualFile
+import com.nekofar.milad.intellij.blitz.BlitzBundle
+import com.nekofar.milad.intellij.blitz.BlitzIcons
+import javax.swing.Icon
+
+class BlitzCliProjectGenerator : NpmPackageProjectGenerator() {
+    private val packageName = "blitz"
+    private val executable = "blitz"
+    private val initCommand = "init"
+
+    override fun getName(): String {
+        return BlitzBundle.message("blitz.project.generator.name")
+    }
+
+    override fun getDescription(): String {
+        return BlitzBundle.message("blitz.project.generator.description")
+    }
+
+    override fun filters(project: Project, baseDir: VirtualFile): Array<Filter> {
+        return emptyArray()
+    }
+
+    override fun customizeModule(p0: VirtualFile, p1: ContentEntry?) {}
+
+    override fun packageName(): String {
+        return packageName
+    }
+
+    override fun presentablePackageName(): String {
+        return BlitzBundle.message("blitz.project.generator.presentable.package.name")
+    }
+
+    override fun getNpxCommands(): List<NpxPackageDescriptor.NpxCommand> {
+        return listOf(NpxPackageDescriptor.NpxCommand(packageName, executable))
+    }
+
+    override fun generateInTemp(): Boolean {
+        return true
+    }
+
+    override fun generatorArgs(project: Project?, dir: VirtualFile?, settings: Settings?): Array<String> {
+        return arrayOf(initCommand, project?.name.toString())
+    }
+
+    override fun getIcon(): Icon {
+        return BlitzIcons.ProjectGenerator
+    }
+}
